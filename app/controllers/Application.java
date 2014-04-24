c1b1594309e4143dd723f7a45840ac79ce62576c
Philip Langer <langer@big.tuwien.ac.at>
2014-04-24 18:09:53 +0200

    Adds a simple example of a template that renders a list of characters

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,8 +1,10 @@
 package controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import play.*;
 import play.mvc.*;
-
 import views.html.*;
 
 public class Application extends Controller {
@@ -22,6 +24,16 @@ public class Application extends Controller {
 		} else {
 			return badRequest("Provide a proper name!");
 		}
+	}
+
+	public static Result listCharacters(String text) {
+		List<Character> characters = new ArrayList<Character>();
+		for (char c : text.toLowerCase().toCharArray()) {
+			if (!characters.contains(c)) {
+				characters.add(c);
+			}
+		}
+		return ok(characterlist.render(text, characters));
 	}
 
 }
