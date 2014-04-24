ef1891cedac11acd98c42a7454dd9c11954e3af8
Philip Langer <langer@big.tuwien.ac.at>
2014-04-24 16:18:51 +0200

    Adds some basic actions and routes

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -7,8 +7,21 @@ import views.html.*;
 
 public class Application extends Controller {
 
-    public static Result index() {
-        return ok(index.render("Your new application is ready."));
-    }
+	public static Result index() {
+		return ok(index.render("Your new application is ready."));
+	}
+
+	public static Result sayHello() {
+		return ok("Hello " + request().remoteAddress());
+	}
+
+	public static Result sayHelloTo(String name) {
+		if (name.toLowerCase().matches("[a-z]+")) {
+			String theName = name.toUpperCase();
+			return ok("Hello " + theName);
+		} else {
+			return badRequest("Provide a proper name!");
+		}
+	}
 
 }
